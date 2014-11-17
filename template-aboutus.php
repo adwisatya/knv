c9546b053e1b581dea1a8ddee87953bfbc3a4b90
Aryya Dwisatya Widigha <a.dwisaty4@yahoo.com>
2014-11-17 16:15:06 +0700

    Inisiasi Termin 3

---
+++ template-aboutus.php
@@ -0,0 +1,41 @@
+<?php
+/**
+
+Template Name: About Us
+
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package web2feel
+ * @since web2feel 1.0
+ */
+
+
+
+get_header(); ?>
+	<div id="feature-section" class="cf">	
+	<div class="latest-head grid_12">
+		<h3>ABOUT US</h3>
+	</div>
+	<!-- edit bagian sini -->
+	<div id="primary" class="content-area container_12">
+		<div id="article-area" class="cf">
+			<div style="margin-left:20px;float:left;margin-right:20px;height:350px;">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/logo/logo.jpg" width="150px" height="150px">
+			</div>
+			<div>
+				<p style="margin-left:20px;width:800px;height:350px;text-align:justify;">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, 
+				</p>
+			</div>
+		</div>
+		<div class="grid_12">
+			<?php kriesi_pagination(); ?>
+		</div>
+	</div><!-- #primary .content-area -->
+<?php get_footer(); ?>
---
+++ template-schedule.php
@@ -0,0 +1,36 @@
+<?php
+/**
+
+Template Name: Schedule
+
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package web2feel
+ * @since web2feel 1.0
+ */
+
+
+
+get_header(); ?>
+	<div id="feature-section" class="cf">
+	<div class="latest-head grid_12">
+		<h3>New arrivals</h3>
+	</div>
+	<!-- edit bagian ini: sync dengan google calendar -->
+	<div id="primary" class="content-area container_12">
+		<div id="article-area" class="cf" align="center">
+			<iframe src="https://www.google.com/calendar/embed?src=a.dwisatya%40yahoo.com&ctz=Asia/Jakarta" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+		</div>
+		<div class="grid_12">
+			<?php kriesi_pagination(); ?>
+		</div>
+	</div><!-- #primary .content-area -->
+
+
+<?php get_footer(); ?>
