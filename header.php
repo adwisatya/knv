c002468215bf1f47d6860c305b2b309177fb6d43
Aryya Dwisatya Widigha <a.dwisaty4@yahoo.com>
2014-12-04 20:46:25 +0700

    deploy to web

--- header.php
+++ header.php
@@ -80,12 +80,12 @@
 					<div id="botmenu">
 						<?php //wp_nav_menu( array( 'container_id' => 'submenu', 'theme_location' => 'primary','menu_id'=>'web2feel' ,'menu_class'=>'sfmenu','fallback_cb'=> 'fallbackmenu' ) ); ?>
 						<a href="http://localhost/KANAVAIA/">Home</a> &nbsp;|&nbsp; 
-						<a href="http://localhost/KANAVAIA/about-us">About Us </a>&nbsp;|&nbsp; 
-						<a href="http://localhost/KANAVAIA/lookbook">LookBook </a>&nbsp;|&nbsp; 
+						<a href="http://kanavaia.com/?page_id=12">About Us </a>&nbsp;|&nbsp; 
+						<a href="http://kanavaia.com/?page_id=18">LookBook </a>&nbsp;|&nbsp; 
 						<a href="http://localhost/KANAVAIA/explorer-review">Explorer Review </a>&nbsp;|&nbsp; 
 						<a href="http://localhost/KANAVAIA/products">Products </a>&nbsp;|&nbsp; 
-						<a href="http://localhost/KANAVAIA/schedule">Schedule </a>&nbsp;|&nbsp; 
-						<a href="http://localhost/KANAVAIA/contact-us">How to Buy</a>
+						<a href="http://kanavaia.com/?page_id=16">Schedule </a>&nbsp;|&nbsp; 
+						<a href="http://kanavaia.com/?page_id=14">How to Buy</a>
 						<hr style="height:3px;color:black;">
 					</div>
 					<div class="searchbox">
