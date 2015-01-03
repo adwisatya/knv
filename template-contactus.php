21203552d49691bb24d405921e141b9a35bd8a52
unknown <rita_miffies@yahoo.co.id>
2015-01-03 20:36:02 +0700

    Form

--- template-contactus.php
+++ template-contactus.php
@@ -107,12 +107,34 @@ p{
 		</tr>
 	</table>
 </p>
+<?php if ( have_posts() ) : ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+<!-- 
+				<?php kriesi_pagination(); ?>
+
+				<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
+
+				<?php get_template_part( 'no-results', 'index' ); ?>
+ -->
+			<?php endif; ?>
+
 			</div>
 		</div>
-		<div class="grid_12">
+		<!-- <div class="grid_12">
 			<?php kriesi_pagination(); ?>
-		</div>
+		</div> -->
 	</div><!-- #primary .content-area -->
 	</div>
-
 <?php get_footer(); ?>
